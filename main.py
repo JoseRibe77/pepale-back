1f29ce0c4521be095837f732db1dadb006988f94
JoseRibe77 <joseribe77@gmail.com>
2025-07-22 15:24:48 -0400

    cambios en el main

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from routes.ventas import router as ventas_router
 from routes.auth import router as auth_router
 from routes.productos import router as productos_router
+from routes.admin import router as admin_router
 
+app.include_router(admin_router)
 app = FastAPI()
 
 app.include_router(ventas_router)
