86d72494a17562265dcb7ce6bcd4155c6551dea4
JoseRibe77 <71667496+JoseRibe77@users.noreply.github.com>
2025-07-22 10:52:10 -0400

    Update productos.py

--- routes/productos.py
+++ routes/productos.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/productos")
+def get_productos():
+    return {"message": "Lista de productos (temporal)"}
 # rutas para productos (crear, listar, IA)
