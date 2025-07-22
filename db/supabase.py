6d9e180e004af0280e8363873c6414a898245201
JoseRibe77 <71667496+JoseRibe77@users.noreply.github.com>
2025-07-22 14:41:45 -0400

    Update supabase.py

--- db/supabase.py
+++ db/supabase.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:tEDqhYChS90yKMR4@db.xwylxosareknuyxhsqgw.supabase.co:5432/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")  # Se espera que la variable esté bien configurada
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
