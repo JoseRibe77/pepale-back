3947fcb57416723e51d9bd5b0d99e65227d9ba11
JoseRibe77 <71667496+JoseRibe77@users.noreply.github.com>
2025-07-22 10:59:08 -0400

    Update ventas.py

--- routes/ventas.py
+++ routes/ventas.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/ventas")
+async def listar_ventas():
+    return {"mensaje": "Listado de ventas"}
 # rutas para ventas y almacenamiento
