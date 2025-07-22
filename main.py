0bef23d03c04fd56018e10d6a66298adaa5e8d05
JoseRibe77 <joseribe77@gmail.com>
2025-07-22 15:30:19 -0400

    main corregido con el error app no found

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from routes.auth import router as auth_router
 from routes.productos import router as productos_router
 from routes.admin import router as admin_router
 
-app.include_router(admin_router)
 app = FastAPI()
+app.include_router(admin_router)
 
 app.include_router(ventas_router)
 app.include_router(auth_router)
